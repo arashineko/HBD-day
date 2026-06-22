7675c759c7ae193a8f53824273843bd80c67ae1f
arashineko <51737385+arashineko@users.noreply.github.com>
2026-06-22 17:47:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const gifStages = [
-    "https://media.tenor.com/EBV7OT7ACfwAAAAj/u-u-qua-qua-u-quaa.gif",    // 0 normal
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAd/chiikawa-hachiware.gif",  // 1 confused
-    "https://media.tenor.com/f_rkpJbH1s8AAAAj/somsom1012.gif",             // 2 pleading
-    "https://media.tenor.com/OGY9zdREsVAAAAAj/somsom1012.gif",             // 3 sad
-    "https://media1.tenor.com/m/WGfra-Y_Ke0AAAAd/chiikawa-sad.gif",       // 4 sadder
-    "https://media.tenor.com/CivArbX7NzQAAAAj/somsom1012.gif",             // 5 devastated
-    "https://media.tenor.com/5_tv1HquZlcAAAAj/chiikawa.gif",               // 6 very devastated
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAC/chiikawa-hachiware.gif"  // 7 crying runaway
+    "https://media.tenor.com/Vex9IWYSUhMAAAAd/jake-the-dog-jake-el-perro.gif",    // 0 normal
+    "https://media.tenor.com/VHAZ97hZHCwAAAAd/adventure-time-jake-the-dog.gif",  // 1 confused
+    "https://media.tenor.com/TO-cV14kFm4AAAAd/sad-tears.gif",             // 2 pleading
+    "https://media.tenor.com/3J0kIEBjplkAAAAd/jake-crying.gif",             // 3 sad
+    "https://media.tenor.com/oAWvKHNMOtIAAAAd/adventuretime-jake.gif",       // 4 sadder
+    "https://media.tenor.com/8wuOy9W1j2AAAAAd/gif.gif",             // 5 devastated
+    "https://media.tenor.com/nqEej6-tgDMAAAAd/jake-angry-jake-el-perro.gif",               // 6 very devastated
+    "https://media.tenor.com/wK_W0bscONwAAAAd/adventure-time-jake.gif"  // 7 crying runaway
 ]
 
 const noMessages = [
